04c6b23642a66b8a4ec120cded9f808f12474d16
Fangpei Cheng <2573547693@qq.com>
2015-11-03 13:34:06 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-	def show
+  def show
         @user = User.find(params[:id])
       end
   def new
-  end
+        @user = User.new
+      end
+  def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+          flash[:success] = "Welcome to the Twitter App!"
+           redirect_to @user# Handle a successful save.
+        else
+            render 'new'   
+        end
+      end
 end
