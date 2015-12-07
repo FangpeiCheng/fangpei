bc51e356c620c6db8b05d79ced9e367ca2e953df
Fangpei Cheng <2573547693@qq.com>
2015-12-07 10:07:15 +0000

    Add title to mocroposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
       before_action :logged_in_user, only: [:create, :destroy]
       before_action :correct_user,   only: :destroy  # NEW LINE
       def create
-        secure_post = params.require(:micropost).permit(:content)
+        secure_post = params.require(:micropost).permit(:title,:content)#new line
         @micropost = current_user.microposts.build(secure_post) 
         if @micropost.save
           flash[:success] = "Micropost created!"
