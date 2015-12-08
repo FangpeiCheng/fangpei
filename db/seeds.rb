b645c24843575fba22db527797d34086d3f3fdae
Fangpei Cheng <2573547693@qq.com>
2015-12-08 19:00:00 +0000

    modify it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # Users
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "example",
+             email: "example@163.com",
+             password:              "123456",
+             password_confirmation: "123456",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
 60.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+  email = "example-#{n+1}@163.com"
+  password = "123456"
   User.create!(name: name,
               email: email,
               password:              password,
